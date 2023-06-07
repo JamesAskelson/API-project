e36a7198ae042f9a62e6c33089cc106ef1c6073a
JamesAskelson <jamesaskelson630@gmail.com>
2023-06-07 18:13:23 -0500

    ok actual phase 1 done not phase 0 lmao

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-
+const apiRouter = require('./api');
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+
+router.use('/api', apiRouter);
 
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -13,5 +15,7 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+
+
 
 module.exports = router;
