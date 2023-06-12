79bce0608a7096de402364ef1f824c8962a6f396
JamesAskelson <jamesaskelson630@gmail.com>
2023-06-12 01:48:57 -0500

    everything for the database done, need to add more seeds

---
+++ backend/db/migrations/20230612060654-create-image.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let options = {};
+if(process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Images', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      reviewId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Reviews"
+        },
+        onDelete: "CASCADE"
+      },
+      spotId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Spots"
+        },
+        onDelete: "CASCADE"
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      preview: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Images"
+    await queryInterface.dropTable('Images');
+  }
+};
---
+++ backend/db/models/spot.js
@@ -0,0 +1,80 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Spot extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Spot.hasMany(models.Review, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        hooks: true
+      })
+      Spot.hasMany(models.Booking, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        hooks: true
+      })
+      Spot.hasMany(models.Image, {
+        foreignKey: 'spotId',
+        onDelete: 'CASCADE',
+        hooks: true,
+        as: "SpotImages"
+      })
+      Spot.belongsTo(models.User, {
+        foreignKey: 'ownerId'
+      })
+    }
+  }
+  Spot.init({
+    ownerId: DataTypes.INTEGER,
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lat: {
+      type: DataTypes.FLOAT(3,7),
+      allowNull: false
+    },
+    lng: {
+      type: DataTypes.FLOAT(3,7),
+      allowNull: false
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+    avgRating: {
+      type: DataTypes.NUMBER,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'Spot',
+  });
+  return Spot;
+};
